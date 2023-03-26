ab19b3fd587bc46d42b0d4959c082230157739b3
王嘉俊 <657447542@qq.com>
2023-03-26 12:49:04 +0800

    feat: remove useless code

--- sumk-form-core/src/main/java/org/yx/sumk/form/bind/property/BeanPropertyHandlerVisitors.java
+++ sumk-form-core/src/main/java/org/yx/sumk/form/bind/property/BeanPropertyHandlerVisitors.java
@@ -1,7 +1,6 @@
 package org.yx.sumk.form.bind.property;
 
 import org.yx.annotation.Bean;
-import org.yx.sumk.form.bind.bean.BeanPropertyHandler;
 import org.yx.sumk.form.bind.bean.FormBeanPropertyHandler;
 
 /**
@@ -21,8 +20,6 @@ public class BeanPropertyHandlerVisitors {
          */
         T visit(Object target);
     }
-
-    public static final PropertyVisitor<BeanPropertyHandler> BEAN_PROPERTY_HANDLER = BeanPropertyHandler::new;
 
     public static final PropertyVisitor<FormBeanPropertyHandler> FORM_BEAN_PROPERTY_HANDLER = FormBeanPropertyHandler::new;
 
